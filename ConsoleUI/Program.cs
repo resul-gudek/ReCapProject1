cdc27be0cbdba2a0fb8350589661b0594853d1db
RESUL GÜDEK <resul_gudek37@hotmail.com>
2021-02-06 20:58:23 +0300

    Framework eklendi

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleUI
 
             foreach (var car in carManager.GetAll())
             {
-                Console.WriteLine(car.Description);
+             Console.WriteLine(car.Description);
             }
         }
     }
