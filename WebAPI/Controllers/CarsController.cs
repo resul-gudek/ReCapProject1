6bababaab4484dffa57a09cc192ff539c10f62e7
RESUL GÜDEK <resul_gudek37@hotmail.com>
2021-02-17 19:46:08 +0300

    WEBAPI

---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        [HttpGet]
+        public List<Car> Get()
+        {
+            return "Merhaba";
+        }
+    }
+}
